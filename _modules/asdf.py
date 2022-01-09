ac66d3b2edc54aeffd566320b0aa8f41620127e6
jeanluc <lkubb@protonmail.com>
2022-01-09 13:56:43 +0100

    Fixes is_version_available, adds info/debug log

--- _modules/asdf.py
+++ _modules/asdf.py
@@ -13,10 +13,13 @@ def __virtual__():
 
 def _which(user=None):
     if e := __salt__["cmd.run_stdout"]("command -v asdf", runas=user):
+        __salt__['log.debug']('Found asdf executable at {}'.format(e))
         return e
     if salt.utils.platform.is_darwin():
         if p := __salt__["cmd.run_stdout"]("brew --prefix asdf", runas=user):
-            return p + "/libexec/bin/asdf"
+            e = p + "/libexec/bin/asdf"
+            __salt__['log.debug']('Found asdf executable at {}'.format(e))
+            return e
     raise CommandExecutionError("Could not find asdf executable.")
 
 
@@ -38,7 +41,7 @@ def is_version_installed(name, version, user=None):
 def is_version_available(name, version, user=None):
     if not is_plugin_available(name, user):
         raise CommandExecutionError('Requested plugin {} is not available in asdf.'.format(name))
-    return name in list_available_versions(name, user)
+    return str(version) in list_available_versions(name, user)
 
 
 def install_plugin(name, user=None):
@@ -46,6 +49,8 @@ def install_plugin(name, user=None):
         raise CommandExecutionError('Requested plugin {} is not available in asdf.'.format(name))
 
     e = _which(user)
+
+    __salt__['log.info']('asdf: Installing plugin {}'.format(name))
 
     # retcode returns shell-style retcode, need inverse
     return not __salt__['cmd.retcode']("{} plugin-add '{}'".format(e, name), runas=user)
@@ -57,14 +62,18 @@ def install_version(name, version, user=None):
 
     e = _which(user)
 
+    __salt__['log.info']('asdf: Installing {} {}'.format(name, version))
+
     return not __salt__['cmd.retcode']("{} install '{}' '{}'".format(e, name, version), runas=user)
 
 
 def remove_plugin(name, user=None):
     if not is_plugin_installed(name, user):
-        return True
+        raise CommandExecutionError('Plugin {} is not installed.'.format(name))
 
     e = _which(user)
+
+    __salt__['log.info']('asdf: Removing plugin {}'.format(name))
 
     return not __salt__['cmd.retcode']("{} plugin-remove '{}'".format(e, name), runas=user)
 
@@ -74,9 +83,11 @@ def remove_version(name, version, user=None):
         raise CommandExecutionError('Plugin {} is not installed.'.format(name))
 
     if not is_version_installed(name, user):
-        return True
+        raise CommandExecutionError('{} version {} is not installed.'.format(name, version))
 
     e = _which(user)
+
+    __salt__['log.info']('asdf: Removing {} {}'.format(name, version))
 
     return not __salt__['cmd.retcode']("{} uninstall '{}' '{}'".format(e, name, version), runas=user)
 
@@ -87,12 +98,15 @@ def update_plugin(name, user=None):
 
     e = _which(user)
 
+    __salt__['log.info']('asdf: Updating plugin {}'.format(name))
+
     return not __salt__['cmd.retcode']("{} plugin-update '{}'".format(e, name), runas=user)
 
 
 def update_plugins(name=None, user=None):
     e = _which(user)
 
+    __salt__['log.info']('asdf: Updating plugins.')
     return not __salt__['cmd.retcode']("{} plugin-update --all".format(e), runas=user)
 
 
@@ -103,13 +117,16 @@ def set_version(name, version, user=None, cwd=''):
     e = _which(user)
 
     if cwd:
+        __salt__['log.info']('asdf: Setting {} {} for {}'.format(name, version, cwd))
         return not __salt__['cmd.retcode']("{} local '{}' '{}'".format(e, name, version), cwd=cwd, runas=user)
 
+    __salt__['log.info']('asdf: Setting {} {} globally'.format(name, version))
     return not __salt__['cmd.retcode']("{} global '{}' '{}'".format(e, name, version), runas=user)
 
 
-def reshim(user=None, cwd=''):
+def reshim(user=None):
     e = _which(user)
+    __salt__['log.info']('asdf: Starting reshim.')
     return not __salt__['cmd.retcode']('{} reshim'.format(e), runas=user)
 
 
@@ -136,13 +153,16 @@ def list_available_plugins(user=None):
 def list_installed_plugins(user=None):
     e = _which(user)
     installed = __salt__['cmd.run_stdout']('{} plugin-list'.format(e), runas=user, raise_err=True)
+    __salt__['log.debug']('asdf: List of installed plugins: {}'.format(installed))
     return installed.splitlines()
 
 
 def list_available_versions(name, user=None):
     e = _which(user)
-    available = __salt__['cmd.run_stdout']('{} list-all \'{}\''.format(e, name), runas=user, raise_err=True)
-    return available.splitlines()
+    available = __salt__['cmd.run_stdout']('{} list-all \'{}\''.format(e, name), runas=user, raise_err=True).splitlines()
+
+    __salt__['log.debug']('asdf: Available versions for {}: {}'.format(name, available))
+    return available
 
 
 def list_installed_versions(name, user=None):
